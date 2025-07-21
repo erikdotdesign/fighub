4309ac9b2675b471531a3f5ba88d004c5e239637
Erik <erik@Mac.lan>
2025-07-21 07:39:56 -0400

    initial commit, esbuild app setup

---
+++ ui-src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    if (typeof parent !== undefined) {
+      parent?.postMessage?.({ pluginMessage: 'hello' }, '*')
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <button
+        onClick={() => {
+          parent?.postMessage?.({ pluginMessage: 'close' }, '*')
+        }}>
+        Close
+      </button>
+    </div>
+  )
+};
+
+export default App;
